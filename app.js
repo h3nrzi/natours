b739def328bd33412e757b44bba2da1882259463
Hossein <rezaeig22@gmail.com>
2023-12-31 15:32:58 +0330

    Handling DELETE Requests

--- app.js
+++ app.js
@@ -87,6 +87,21 @@ app.patch('/api/v1/tours/:id', (req, res) => {
 	});
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	if (id >= tours.length) {
+		return res.status(404).json({
+			status: 'fail',
+			message: 'Invalid ID.'
+		});
+	}
+
+	res.status(204).json({
+		status: 'success',
+		data: null
+	});
+});
+
 app.listen(port, () => {
 	console.log(`App running on ${port}...`);
 });
