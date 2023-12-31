31745062d4a5a76ff197264c3319c21aaaf53d4b
Hossein <rezaeig22@gmail.com>
2023-12-31 15:10:49 +0330

    Responding to URL Parameters

--- app.js
+++ app.js
@@ -30,6 +30,29 @@ app.get('/api/v1/tours', (req, res) => {
 	});
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+	// console.log(req.params);
+	// console.log(parseInt(req.params.id));
+
+	const id = parseInt(req.params.id);
+	const tour = tours.find((tour) => tour.id === id);
+
+	// if (id >= tours.length) {
+	if (!tour) {
+		return res.status(404).json({
+			status: 'fail',
+			message: 'Invalid ID.'
+		});
+	}
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			tour: tour
+		}
+	});
+});
+
 app.post('/api/v1/tours', (req, res) => {
 	// console.log(req.body);
 
