753ea583a54ecbe5e93a0b917f8ef3dabe1bda78
Hossein <rezaeig22@gmail.com>
2024-01-01 11:40:22 +0330

    Setting up ESLint + Prettier in VS Code

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
 console.log('ENV:', app.get('env'));
 
 const port = process.env.PORT || 3000;
