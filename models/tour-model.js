e9de1330528c98d7ef30ff46e69404f62c9286c8
Hossein <rezaeig22@gmail.com>
2024-01-03 12:15:19 +0330

    Document Middleware

--- models/tour-model.js
+++ models/tour-model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
 	{
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
 			unique: true,
 			trim: true
 		},
+		slug: String,
 		duration: {
 			type: Number,
 			required: [true, 'A tour must have a duration']
@@ -64,5 +66,23 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function() {
 	return this.duration / 7;
 });
+
+// Document Middleware: Runs ONLY before save() and create()
+tourSchema.pre('save', function(next) {
+	this.slug = slugify(this.name, { lower: true });
+	next();
+});
+
+/*** 
+tourSchema.pre('save', function(next) {
+	console.log('Will save document...');
+	next();
+});
+
+tourSchema.post('save', function(doc, next) {
+	console.log(doc);
+	next();
+});
+***/
 
 module.exports = mongoose.model('Tour', tourSchema);
