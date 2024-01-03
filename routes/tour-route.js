8fac48b72ec2071d7332e26c3348cd47bd10ea90
Hossein <rezaeig22@gmail.com>
2024-01-03 07:52:09 +0330

    Making the API Better Aliasing

--- routes/tour-route.js
+++ routes/tour-route.js
@@ -5,6 +5,11 @@ const router = express.Router();
 
 router
 	//
+	.route('/top-5-cheap')
+	.get(tourController.aliasTopTours, tourController.getAllTours);
+
+router
+	//
 	.route('/')
 	.get(tourController.getAllTours)
 	.post(tourController.createTour);
