11071de7658d1f46ede646ebde3613bf4da27df9
Hossein <rezaeig22@gmail.com>
2024-01-04 15:51:08 +0330

    Errors During Development vs Production

--- controllers/error-controller.js
+++ controllers/error-controller.js
@@ -1,11 +1,40 @@
-function globalErrorHandler(err, req, res, next) {
-	const statusCode = err.statusCode || 500;
-	const status = err.status || 'error';
-
-	res.status(statusCode).json({
-		status: status,
-		message: err.message
+function sendErrorDev(err, res) {
+	res.status(err.statusCode).json({
+		status: err.status,
+		error: err,
+		message: err.message,
+		stack: err.stack
 	});
+}
+
+function sendErrorProd(err, res) {
+	// Operational or Trusted Errors
+	if (err.isOperational) {
+		res.status(err.statusCode).json({
+			status: err.status,
+			message: err.message
+		});
+
+		// Programming or other Unknown Errors
+	} else {
+		console.error('🔷 ERROR', err);
+
+		res.status(500).json({
+			status: 'error',
+			message: 'Something went very wrong!'
+		});
+	}
+}
+
+function globalErrorHandler(err, req, res, next) {
+	err.statusCode = err.statusCode || 500;
+	err.status = err.status || 'error';
+
+	if (process.env.NODE_ENV === 'development') {
+		sendErrorDev(err, res);
+	} else if (process.env.NODE_ENV === 'production') {
+		sendErrorProd(err, res);
+	}
 }
 
 module.exports = globalErrorHandler;
