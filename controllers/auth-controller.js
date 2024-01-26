af5385b2e07c10cc4adb39a302e9dd5402494a0f
Hossein <rezaeig22@gmail.com>
2024-01-26 21:19:26 +0330

    Creating New Users

---
+++ controllers/auth-controller.js
@@ -0,0 +1,16 @@
+const User = require('../models/user-model');
+const catchAsync = require('../utils/catch-async');
+
+exports.signup = catchAsync(async (req, res, next) => {
+	const newUser = await User.create({
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password,
+		passwordConfirm: req.body.passwordConfirm
+	});
+
+	res.status(201).json({
+		status: 'success',
+		user: newUser
+	});
+});
--- routes/user-route.js
+++ routes/user-route.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const userController = require('../controllers/user-controller');
+const authController = require('../controllers/auth-controller');
 
 const router = express.Router();
 
+router.post('/signup', authController.signup);
+
 router
-	//
 	.route('/')
 	.get(userController.getAllUsers)
 	.post(userController.createUser);
 
 router
-	//
 	.route('/:id')
 	.get(userController.getUser)
 	.patch(userController.updateUser)
