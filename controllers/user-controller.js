75de810b4d29b257f1fe72a07e9607d626574ec8
Hossein <rezaeig22@gmail.com>
2024-02-21 11:42:54 +0330

    Updating the Current User Data

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,30 @@
 const User = require('../models/user-model');
+const AppError = require('../utils/app-error');
 const catchAsync = require('../utils/catch-async');
+const filterObj = require('../utils/filterObj');
+
+// UPDATES THE CURRENT USER'S PROFILE DETAILS EXCEPT PASSWORD
+exports.updateMe = catchAsync(async (req, res, next) => {
+	// 1) Check if password-related fields are present in the request body
+	if (req.body.password || req.body.passwordConfirm)
+		return next(
+			new AppError(
+				'This route is not for password updates. Please use the /updateMyPassword route.',
+				400
+			)
+		);
+
+	// 2) Filter out unwanted fields from the request body
+	const filteredBody = filterObj(req.body, 'name', 'email');
+	// Find and update user's information
+	const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+		new: true, // Return the updated document
+		runValidators: true // Run validators on update
+	});
+
+	// 3) Respond with updated user data
+	res.status(200).json({ status: 'success', data: { updatedUser } });
+});
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
 	const users = await User.find();
